a41d9a41ca045ac451077addbc4bf6031bf0cbb0
ragulm <ragul.lakshmanan@gmail.com>
2026-01-14 18:05:59 +0530

    Initial commit

---
+++ Practice/Network/NetworkHelper.swift
@@ -0,0 +1,39 @@
+//
+//  NetworkHelper.swift
+//  Practice
+//
+//  Created by Ragul Lakshmanan on 08/01/26.
+//
+
+import Foundation
+
+struct Person: Codable {
+    var name: String
+}
+
+
+class NetworkHelper {
+    
+    
+    func fetchRequest() async throws -> [Person] {
+        let request = URLRequest(url: URL(string: "https://api.shell-ai.com/recommendations")!)
+        let (data, _) = try await URLSession.shared.data(for: request)
+        let personData = try JSONDecoder().decode([Person].self, from: data)
+        return personData
+    }
+    
+    func webSocketTry() {
+        let request = URLRequest(url: URL(string: "https://api.shell-ai.com/recommendations")!)
+        let task = URLSession.shared.webSocketTask(with: URL(string: "https://api.shell-ai.com/recommendations")!)
+        task.resume()
+        
+        task.receive { (result) in
+        switch result {
+            case .success(let message):
+                print(message)
+            case .failure(let error):
+                print(error)
+            }
+        }
+    }
+}
---
+++ Practice/Practice.swift
@@ -0,0 +1,9 @@
+//
+//  Practice.swift
+//  Practice
+//
+//  Created by Ragul Lakshmanan on 08/01/26.
+//
+
+import Foundation
+
